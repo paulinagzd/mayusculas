066947ba0db1add8ebe90caac2935297e697c14f
robygonzalez <a01570010@itesm.mx>
2020-04-19 13:53:00 -0500

    add db.plist and sample data

--- Mayusculas/ViewController.swift
+++ Mayusculas/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var arrDatos: NSArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let path = Bundle.main.path(forResource: "db", ofType: "plist")
+        arrDatos = NSArray(contentsOfFile: path!)
+        //let dato = arrDatos[0] as! NSDictionary   -- tienes que guardar el dato que estas guardando en otra variables
+        //lTest.text = dato["letraAPoner"] as? String asi accesas los valores, solo cambien lo que está dentro de las []
     }
+    
 
 
 }
