637b74a99c724bd439e34ac18825bc29f3863d64
isabelcruz816 <isabel.cruz81699@gmail.com>
2020-05-10 21:14:51 -0500

    disabled back nav in last view

--- Mayusculas/ScoreViewController.swift
+++ Mayusculas/ScoreViewController.swift
@@ -15,9 +15,12 @@ class ScoreViewController: UIViewController, MFMailComposeViewControllerDelegate
     var puntos : Int!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
+        self.navigationItem.hidesBackButton = true
+        self.navigationController?.navigationItem.backBarButtonItem?.isEnabled = false
+        self.navigationController!.interactivePopGestureRecognizer!.isEnabled = false;
+        
         lbPuntaje.text = String(puntos!) + "puntos"
+        super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
